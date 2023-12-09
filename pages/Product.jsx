c5a270bc2e8a6f25baae881772bdf4a7b5c09dce
Marco Boschetti <marcob.brl@gmail.com>
2023-12-09 17:10:18 -0300

    test 2

--- pages/Product.jsx
+++ pages/Product.jsx
@@ -1,10 +1,56 @@
 import styles from './Product.module.css';
-import produtos from '../products.json';
 import images from '../products-media.json';
 import { useParams } from 'react-router-dom';
 
 const Product = () => {
   const { id } = useParams();
+
+  const produtos = [
+    {
+        "id": 1,
+        "media": ["/camiseta-mc.svg", "/camiseta-mc-2.svg", "/camiseta-mc-3.svg", "/camiseta-mc-4.svg"]
+    },
+    {
+        "id": 2,
+        "media": ["/camisa-social.svg", "/camisa-social-2.svg", "/camisa-social-3.svg", "/camisa-social-4.svg", "/camisa-social-5.svg"]
+    },
+    {
+        "id": 3,
+        "media": ["/camiseta-ml.svg", "/camiseta-ml-2.svg", "/camiseta-ml-3.svg", "/camiseta-ml-4.svg", "/camiseta-ml-5.svg"]
+    },
+    {
+        "id": 4,
+        "media": ["/jaleco-brim.svg", "/jaleco-brim-2.svg", "/jaleco-brim-3.svg", "/jaleco-brim-4.svg"]
+    },
+    {
+        "id": 5,
+        "media": ["/avental-napa.svg", "/avental-napa-2.svg", "/avental-napa-3.svg", "/avental-napa-4.svg"]
+    },
+    {
+        "id": 6,
+        "media": ["/calca-brim.svg", "/calca-brim-2.svg", "/calca-brim-3.svg", "/calca-brim-4.svg"]
+    },
+    {
+        "id": 7,
+        "media": ["/jaqueta.svg", "/jaqueta-2.svg", "/jaqueta-3.svg", "/jaqueta-4.svg", "/jaqueta-5.svg"]
+    },
+    {
+        "id": 8,
+        "media": ["/jaleco-fem.svg", "/jaleco-fem-2.svg", "/jaleco-fem-3.svg", "/jaleco-fem-4.svg", "/jaleco-fem-5.svg", "/jaleco-fem-6.svg"]
+    },
+    {
+        "id": 9,
+        "media": ["/jaleco-masc.svg", "/jaleco-masc-2.svg", "/jaleco-masc-4.svg"]
+    },
+    {
+        "id": 10,
+        "media": ["/jaleco-fem-simples.svg", "/jaleco-fem-simples-2.svg", "/jaleco-fem-simples-3.svg"]
+    },
+    {
+        "id": 11,
+        "media": ["/colete-obras.svg", "/colete-obras-2.svg", "/colete-obras-3.svg"]
+    }
+]
 
   const produto = produtos.find((produto) => produto.id === parseInt(id, 10));
 
